3a434a1be5a62a29ed5c595f7f97c82f343dec04
TENTHER101 <133138386+TENTHER101@users.noreply.github.com>
2024-01-30 15:05:40 +0000

    Update .replit and main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello, world")
+# importing ply(python, lex, and yacc)
+from ply import lex, yacc
+
+# list of tokens, this is always required when using ply
+tokens = (
+    'PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE', 'LPAREN', 'RPAREN', 
+    'NUMBER', 'DECIMAL', 'TEXT', 'BOOLEAN', 'ID', 
+    'OUTPUT', 'SET', 
+    'TO'
+)
