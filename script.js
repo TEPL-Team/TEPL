067cf5aa6f8ab4e10f21c9964aa93d2bde54aef2
Valuex <133138386+TENTHER101@users.noreply.github.com>
2024-03-08 14:29:39 +0000

    Update web

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-import { Analytics } from "@vercel/analytics/react"; 
+import { Analytics } from "@vercel/analytics/react";
 //importing vercel analystics ^
+
+var downloads = {
+    v0750: document.getElementById("download-v-0750"),
+};
+
+downloads.v0750.addEventListener("click", function () {
+    alert("This Download is not yet available, come back another time!");
+});
