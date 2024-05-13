550c6a8adc78fb1fd2e4499553a4a5602303064f
Valuex <133138386+TENTHER101@users.noreply.github.com>
2024-05-13 16:49:54 +0000

    Minor update to main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from lexer import lexer
 from gui import run
 from transpiler import transpile
 
-__version__ = 'v1.80.00'
+__version__ = 'v1.92.06'
 __mode__ = 'execute'  # 'execute' | 'transpile'|'parse'|'tokenize'
 
 
